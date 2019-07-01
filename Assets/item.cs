9ba0d79adc3b5c28af0d5c5667e1d70343bd4382
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-01 20:31:12 -0300

    continuação do inventorio

--- Assets/item.cs
+++ Assets/item.cs
@@ -1,42 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems; //para q serve o UnityEngine.EventSystems?
+using UnityEngine.EventSystems; //para q serve o UnityEngine.EventSystems?  //o q significa aqui a espressão importar?
 
-public class Item : MonoBehaviour, IDragHandler, IEndDragHandler //o IDragHandler e o IEndDragHandler servem para quando o player arrastar os itens do inventario, ele pega-los? 
+public class Item : MonoBehaviour, IDragHandler, IEndDragHandler //o IDragHandler e o IEndDragHandler servem para quando o player arrastar os itens do inventario? mas eles tb colocam os itens em outro lugar? 
 {
     private Vector3 LastTransform;
-    public int id;
-
-    public void OnDrag(PointerEventData eventData)
+    public int id;//id?
+    public Slot inSlot;
+    
+   
+    public void OnDrag(PointerEventData eventData)//?
     {
-        transform.position = Input.mousePosition;     
+        transform.position = Input.mousePosition;//?    
     }
 
 
     public void OnEndDrag(PointerEventData eventData)
-    {
+    {                                                         //o q significa aqui a espressão importar?
         int t = -1;
-        Slot[] slots = FindObjectsOfType<Slot>(); //o q significa aqui a espressão importar?  //aqui esta procurando objetos do tipo slot e salvando na variavel slots?esta parte é um array?
-
+        Slot[] slots = FindObjectsOfType<Slot>();   //aqui esta procurando objetos do tipo slot e salvando na variavel slots?  //esta parte é um array? //como se sabe se um objeto é do tipo Slot?
+                                        
+                                            //este Slot com s maiusculo é apenas o tipo de variavel q o slot é? não é o outro script?
 
         for(int i=0; i<slots.Length; i++)   //o q é o slots.Length?
         {
             if(slots[i].selected==true)  //o [i]serve para representar a posição do objeto do tipo Slot? //o q quer dizer o slots[i].selected==true? 
             {
-                t = i;//?
+                t = i;// se t é igual a i, então daria no mesmo escrever if(slots[t].selected==true)?
                 break;  //este break é pq não tem um else depois deste if?
             }                
 
 
         }   
-        if(t>=0) //?
+        if(t>=0) // ?
         {
-            if (slots[t].id == -1)//aqui o slots[t] se refere a uma posição expecifica no array slots?  //para q serve o .id==-1?
+            if (slots[t].id == -1)//aqui o slots[t] se refere a uma posição expecifica no array slots?  //para q serve o .id==-1?  // o -1 sgnifica q essa posição esta vazia, mas não posso usar 0 no lugar do -1?
             {
-                transform.position = slots[t].transform.position; //slots[t]?
+                inSlot.id = -1;
+                inSlot.Count = 0; //pq aqui não aceita o count com c minusculo?
+                transform.position = slots[t].transform.position; //?
                 LastTransform = transform.position;//?
+                inSlot = slots[t];
                 slots[t].id = id;//?
+            }
+            else
+            {
+                transform.position = LastTransform;
             }
         }
         else
@@ -56,5 +66,5 @@ public class Item : MonoBehaviour, IDragHandler, IEndDragHandler //o IDragHandle
     void Update()
     {
         
-    }
+    }   
 }
--- Assets/slot.cs
+++ Assets/slot.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;//para q serve o using UnityEngine.UI? 
 
-public class slot: MonoBehaviour
+public class Slot: MonoBehaviour
 {
     public int id = -1; //-1 quer diser q não tem itens nele
-    public int menuCount=0;
+    public int Count=0; // aqui significa q não tem nenhum item dentro do inventório?
     public bool selected = false;
 
 
